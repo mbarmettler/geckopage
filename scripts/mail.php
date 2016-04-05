b931bc52e3967f856dbc07c47071767cd86c2611
Mike Barmettler <mike.barmettler@chainiq.com>
2016-04-05 11:28:16 +0200

    created mail functionallity with php

---
+++ scripts/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+if($_POST)
+{
+    $name = $_POST['form_name'];
+    $email = $_POST['form_email'];
+    $message = $_POST['form_msg'];
+
+	//send email
+    mail("greenlion1010@gmail.com", "GeckoPage Contact Form" .$email, $message);
+}
+?>
